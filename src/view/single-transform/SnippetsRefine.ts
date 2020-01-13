19606c4dba833ec3628c56852e5385acd5afbe4a
刘淼 <liumiao@cvte.com>
2020-01-13 10:57:47 +0800

    [fix] 优化 snippets 的格式化

--- src/view/single-transform/SnippetsRefine.ts
+++ src/view/single-transform/SnippetsRefine.ts
@@ -3,11 +3,13 @@ import SingleTransform from "./single-transform";
 export default new SingleTransform({
   placeholder: "将代码转换为标准的 Snippets",
   transForm: (str = "") => {
-    let rus = '"prefix" : "" \n, "body" : [\n"';
+    const indent1 = "    ";
+    const indent2 = "      ";
+    let rus = `${indent1}"prefix" : "",\n${indent1}"body" : [\n${indent2}"`;
     for (let x of str) {
       switch (x) {
         case "\n":
-          rus += '"\n,"';
+          rus += `"\n${indent2},"`;
           break;
         case "\t":
           rus += "\\r";
@@ -19,7 +21,7 @@ export default new SingleTransform({
           rus += x;
       }
     }
-    rus += '"\n]';
+    rus += `"\n${indent1}],`;
     return rus;
   },
 });
